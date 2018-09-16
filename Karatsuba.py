b415419ff5a0f6b053f29ca3d84b71f486da5229
louis.yl.wang <louis.yl.wang@outlook.com>
2018-09-16 23:57:18 +0800

    wrote a readme file

---
+++ Karatsuba.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 16 18:20:06 2018
+
+@author: louis
+"""
+x = 3141592653589793238462643383279502884197169399375105820974944592
+y = 2718281828459045235360287471352662497757247093699959574966967627
+
+
+
+def Karatsuba(x,y):
+    
+    if x<10 or y<10:
+        return x*y   
+    
+    else:
+        prior_n = max(len(str(x)), len(str(y)))
+        prior_n = int(prior_n)
+        n = prior_n//2
+    
+        a = x//(10**n)
+        b = x%(10**n)
+        c = y//(10**n)
+        d = y%(10**n)
+        
+        ac = Karatsuba(a,c)
+        bd = Karatsuba(b,d)
+        ad_bc = Karatsuba(a+b,c+d)-ac-bd        
+        
+        if prior_n%2 == 0: 
+            result = (10**prior_n)*ac+(10**n)*ad_bc+bd
+        else:
+            result = (10**(prior_n-1))*ac+(10**n)*ad_bc+bd
+             
+        return result
+    
+    
+def chaishu(x):
+    
+    if x<10:
+        return [x]   
+    
+    else:
+        prior_n = int(len(str(x)))
+        n = prior_n//2
+    
+        a = x//(10**n)
+        b = x%(10**n)
+        
+        result = [chaishu(a),chaishu(b)]
+        
+        return result
+
+Karatsuba(x,y)
