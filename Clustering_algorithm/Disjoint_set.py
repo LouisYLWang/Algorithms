72fa2e27754232fdb5538ca2f4a5090174663aa5
louis.yl.wang <louis.yl.wang@outlook.com>
2019-04-30 12:22:13 +0800

    finish disjoint_set

--- Clustering_algorithm/Disjoint_set.py
+++ Clustering_algorithm/Disjoint_set.py
@@ -1,4 +1,6 @@
-class Disjoint_set():
+from collections import Iterator, Iterable
+
+class Disjoint_set(Iterable):
     def __init__(self):
         self.head = None
         self.tail = None
@@ -12,39 +14,80 @@ class Disjoint_set():
         element.set = self
 
     def __str__(self):
-        return str([self.head.label, self.tail.label])
+        if not self.is_empty():
+            return str([self.head.label, self.tail.label])
+        else:
+            return 'empty set'
     
     def get_elements(self):
-        elements_ls = [self.head.label]
-        ele = self.head
-        while ele.next:
-            elements_ls.append(ele.next.label)
-        return elements_ls
+        if not self.is_empty():
+            elements_ls = [self.head.label]
+            ele = self.head
+            while ele.next:
+                elements_ls.append(ele.next.label)
+                ele = ele.next
+            return elements_ls
+        else:
+            return []
 
+    def get_len(self):
+
+        if not self.is_empty():
+            set_len = 1
+            ele = self.head
+            while ele.next:
+                set_len+=1
+                ele = ele.next
+            return set_len
+        else:
+            return 0
+
+    def is_empty(self):
+        return self.head == None
+
+    def __iter__(self):
+        if not self.head:
+            return
+        cur = self.head
+        yield cur
+        while cur.next:
+            cur = cur.next
+            yield cur
+    
 
 class Element():
     def __init__ (self, label):
         self.set = None
-        self.next = None
+        self.next = False
         self.label = label
 
-    def update_set(self, set):
-        self.set = set
-
-    def find_set(self):
-        print(self.set.get_elements)
-        return self.set
+    def show_set(self):
+        return(self.set.get_elements())
 
     def __str__(self):
         return str(self.label)
 
 
-vertices = [Element(i) for i in range(10)]
-set1 = Disjoint_set()
-set1.add_element(vertices[0])
-set1.add_element(vertices[8])
-set1.add_element(vertices[3])
-print(set1)
-#print(vertices[0].find_set())
-print(vertices[0].next)
-#set1.get_elements()
+def union(set1, set2):
+    if set1.get_len() > set2.get_len():
+        max_set = set1
+        min_set = set2
+    else:
+        min_set = set1
+        max_set = set2
+
+    max_set.tail.next = min_set.head
+    max_set.tail = min_set.tail
+
+    cur = min_set.head
+    cur.set = max_set
+    while cur.next:
+        cur = cur.next
+        cur.set = max_set
+
+    #for ele in min_set:
+    #    ele.set = max_set
+    min_set.head = None
+    min_set.tail = False
+    return max_set
+
