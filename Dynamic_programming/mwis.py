8cc7ac503c96cfbae1577197b52af5f9a4b0914f
louis.yl.wang <louis.yl.wang@outlook.com>
2019-05-08 21:29:57 +0800

    finish maximum weighted independent set

--- Dynamic_programming/mwis.py
+++ Dynamic_programming/mwis.py
@@ -4,26 +4,22 @@ nodes = [int(line) for line in lines[1:]]
 node_num = int(lines[0])
 subprob_cache = [0 for i in range(node_num)]
 
-subprob_cache[1] = nodes[1]
+subprob_cache[1] = nodes[0]
 res_set = set()
 
 for i in range(2,node_num):
-    subprob_cache[i] = max(subprob_cache[i-1],subprob_cache[i-2] + nodes[i])
+    subprob_cache[i] = max(subprob_cache[i-1],subprob_cache[i-2] + nodes[i-1])
 
-#subprob_cache
 i = node_num - 1
 
 while i >=1:
-    if subprob_cache[i-1] >= subprob_cache[i-2] + nodes[i]:
+    if subprob_cache[i-1] >= subprob_cache[i-2] + nodes[i-1]:
         i-=1
     else:
-        res_set.add(nodes[i])
+        res_set.add(i)
         i-=2
 
 def check_in_set(index):
-    return nodes[index-1] in res_set
+    return i in res_set
 
-list(map(check_in_set, [1, 2, 3, 4, 17, 117, 517, 997]))
-
-for i in [1, 2, 3, 4, 17, 117, 517, 997]:
-    print(nodes[i-1] in res_set)
+print(list(map(check_in_set, [1, 2, 3, 4, 17, 117, 517, 997])))
