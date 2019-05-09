d1c42fe84e089f2155ce7cbde0a23b4cfec24848
louis.yl.wang <louis.yl.wang@outlook.com>
2019-05-10 01:02:46 +0800

    finish knapsack easy version

--- Knapsack/Knapsack1.0.py
+++ Knapsack/Knapsack1.0.py
@@ -1,15 +1,17 @@
 import numpy as np
 file_name = "knapsack1.txt"
+#file_name = "test_case1.txt"
+
 file = open(file_name)
 [w_num, n_num]  = map(int, file.readline().split())
 
 A = np.zeros(shape = (w_num + 1, n_num + 1))
 
 
-for n in range(1,n_num + 2):
+for n in range(1,n_num + 1):
     [v_cur, w_cur] = map(int, file.readline().split())
-    print(v_cur, w_cur) 
     A[0:w_cur, n] = 0
-    for w_j in range(w_cur + 1, w_num + 2):
-        A[w_j, n] = max(A[w_j - w_cur, n-1] + n, A[w_j, n-1])
+    for w_j in range(w_cur , w_num + 1):
+        A[w_j, n] = max(A[w_j - w_cur, n-1] + v_cur, A[w_j, n-1])
                 
+print(A[w_num, n_num])
