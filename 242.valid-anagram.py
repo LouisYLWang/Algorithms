c712fc8a92f5e32d49f969858724af54f47cc116
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-07 00:59:01 +0800

    update 242 Valid Anagram

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -4,6 +4,7 @@
 # [242] Valid Anagram
 #
 class Solution(object):
+    '''
     def isAnagram(self, s, t):
         """
         :type s: str
@@ -19,6 +20,12 @@ class Solution(object):
         s_.sort()
         t_.sort()
         return s_ == t_
-
-
+    '''
+    def isAnagram(self, s, t):
+        dic1, dic2 = [0]*26, [0]*26
+        for item in s:
+            dic1[ord(item)-ord('a')] += 1
+        for item in t:
+            dic2[ord(item)-ord('a')] += 1
+        return dic1 == dic2
 
